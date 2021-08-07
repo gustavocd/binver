0ac2783293f3b1dad96afd7cf0bfe913f7e60955
Gustavo Castillo <gcdcoder@gmail.com>
2021-08-07 11:49:21 -0500

    Add ASCII string

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/gustavocd/binver
 
 go 1.16
+
+require github.com/morikuni/aec v1.0.0 // indirect
--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/morikuni/aec"
 )
 
 var (
@@ -11,12 +13,27 @@ var (
 	version   string
 )
 
+const binverFigletStr = `
+_     _                     
+| |__ (_)_ ____   _____ _ __ 
+| '_ \| | '_ \ \ / / _ \ '__|
+| |_) | | | | \ V /  __/ |   
+|_.__/|_|_| |_|\_/ \___|_|   
+                             
+`
+
+func printASCIIArt() {
+	binverLogo := aec.LightGreenF.Apply(binverFigletStr)
+	fmt.Println(binverLogo)
+}
+
 func main() {
 	displayVersion := flag.Bool("version", false, "Display version and exit")
 
 	flag.Parse()
 
 	if *displayVersion {
+		printASCIIArt()
 		fmt.Printf("Version:\t%s\n", version)
 		fmt.Printf("Build time:\t%s\n", buildTime)
 		os.Exit(0)
